cf3e3601d8402a3d80480487cf349efdab4d83ac
1409 <olli-91-@hotmail.de>
2013-12-03 13:47:37 +0100

    edit link Integriert

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Todoapp::Application.routes.draw do
-  resources :tasks, except: [:show]
+  resources :taks, except: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
