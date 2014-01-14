f3a8267590ec3d1512c2dfa1b8079900cdc6ff18
1409 <olli-91-@hotmail.de>
2014-01-14 15:12:49 +0100

    Username zu User hinzugefügt username ist durch bronko erstezt worden

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :taks
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+	validates :username, presence:  true, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
