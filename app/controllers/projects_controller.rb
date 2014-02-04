41b9b332afb163ea4630852ee26c17ebc7861f20
1409 <olli-91-@hotmail.de>
2014-02-04 19:08:21 +0100

    Create Project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,5 +2,13 @@ class ProjectsController < ApplicationController
 	before_action :authenticate_user!
   def index
   @projects = Project.all
+  def create
+  @project = Project.new(project_params)
+  @project.save
+	end
 	end
 end
+private
+    def project_params
+      params.require(:project).permit(:name)
+    end 
